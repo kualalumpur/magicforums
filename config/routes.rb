2ce7ae05aa6d6910ee8f3c0b323a9770dd5939c5
Terrence <chongweijie88@MacBook-Pro-2.local>
2016-08-04 17:51:09 +0800

    complete topics and posts page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'landing#index'
   get :about, to: 'static_pages#about'
-  resources :topics
+  resources :topics, except: [:show] do
+    resources :posts, except: [:show]
+  end
+
   get :testindex, to: 'landing#testindex' # terrence's testing
   get :test, to: 'test#testing' # terrence's testing
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
